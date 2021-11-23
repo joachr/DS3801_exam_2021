32cb29b00a76c470c494fd35b5d7b55818102d81
Joachim AC <joacchri@gmail.com>
2021-11-23 22:56:52 +0000

    changes to responsiveness footer

--- src/components/HeaderAndFooter/Footer.js
+++ src/components/HeaderAndFooter/Footer.js
@@ -22,24 +22,23 @@ function Footer() {
                 width:'100%',
                 height: '120px',
                 backgroundColor: '#FFD148',
-                marginTop: '150px'
             }}>
                 <Box style={{
                     position: 'relative',
                     height: '100%',
                     backgroundColor: '#006357',
-                    left: '30px'
                 }}>
                     <div style={{
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
-                        marginTop: '1rem',
-                        width: '280px',
+                        width: '180px',
                     }}>
                         <ul style={{
                             listStyle: 'none',
                             color: 'white',
+                            marginLeft: '1rem',
+                            marginRight: '1rem'
                         }}>
                             <li style={{
                                 marginTop: '0.5rem'
@@ -51,13 +50,17 @@ function Footer() {
                                 marginTop: '0.5rem'
                             }}>Adresse: Strandveien 4, 0000, Strand</li>
                         </ul>
-
                     </div>
                 </Box>
-
-                <FacebookIcon style={{marginTop: '20px' ,marginLeft: '30px', fontSize: '70px', color: '#4267B2'}}/>
-                <InstagramIcon style={{marginTop: '20px' ,fontSize: '70px', color: '#E1306C'}}/>
-                <TwitterIcon style={{marginTop: '20px' ,fontSize: '70px', color: '#1DA1F2'}}/>
+                
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}>
+                <FacebookIcon style={{ marginLeft: '10px', fontSize: '40px', color: '#4267B2'}}/>
+                <InstagramIcon style={{marginLeft: '10px',  fontSize: '40px', color: '#E1306C'}}/>
+                <TwitterIcon style={{marginLeft: '10px',  fontSize: '40px', color: '#1DA1F2'}}/>
+                </div>
             </BottomNavigation>  : (
                 <BottomNavigation style={{
                     position:'fixed',
