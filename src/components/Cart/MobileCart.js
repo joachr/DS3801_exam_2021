0feaf770d5ef4bc171e4ec997325fa854c91a64c
siggenpiggen <74364065+siggenpiggen@users.noreply.github.com>
2021-11-23 22:57:46 +0100

    added mobilecart and mediaQuery to cartModal

---
+++ src/components/Cart/MobileCart.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import { useCart } from "react-use-cart";
+import {Typography} from "@material-ui/core";
+import Button from "@mui/material/Button";
+import {makeStyles} from "@material-ui/styles";
+import Divider from "@material-ui/core/Divider";
+
+
+const useStyleCart = makeStyles(() => ({
+	textCartContainer: {
+		
+		display: "flex",
+		width: "100%",
+		textAlign: "right",
+		bottom: "4rem",
+		left: "12rem",
+	}
+}));
+
+const useStyleTableRow = makeStyles(() => ({
+	tableRowContainer: {
+		padding: "15px",
+		margin: "10px",
+		height: "8rem",
+		width: "100%",
+	}
+}));
+
+const useStyleTitle = makeStyles(() => ({
+	titleStyling: {
+		position: "relative",
+		left: "14rem",
+		bottom: "120px",
+	}
+}));
+
+const useStyleButton = makeStyles(() => ({
+	buttonStyling: {
+		position: "relative",
+		display: "flex",
+		left: "0",
+		bottom: "",
+	}
+}));
+
+const useStyleTotalPrice = makeStyles(() => ( {
+	totalPriceStyling: {
+		position: "relative",
+		left: "9rem",
+	}
+}));
+
+const useStyleDivider = makeStyles(() => ({
+	dividerStyling: {
+		position: "absolute",
+		width: "100%",
+		height: "10px",
+		right: "15rem",
+	}
+}));
+
+const MobileCart = () => {
+
+	const { textCartContainer } = useStyleCart();
+	const { tableRowContainer } = useStyleTableRow();
+	const { titleStyling } = useStyleTitle();
+	const { totalPriceStyling } = useStyleTotalPrice();
+	const { dividerStyling } = useStyleDivider();
+	const { buttonStyling } = useStyleButton();
+
+	const {
+		isEmpty,
+		totalUniqueItems,
+		items,
+		totalItems,
+		cartTotal,
+		updateItemQuantity,
+		removeItem,
+		emptyCart,
+	} = useCart();
+
+	if (isEmpty) return <Typography variant={"h5"}>Fitta er tom</Typography>
+
+	return (
+
+		<section>
+			<div>
+				<div>
+					<Typography variant={"h6"}>Handlekurv ({totalUniqueItems}) Totalt fitte ({totalItems})</Typography>
+					<div>
+						<div>
+							{items.map((item, index) => {
+								return(
+									<div className={tableRowContainer} key={index}>
+										<div>
+											<img src={item.img} style={{height: "7rem"}}/>
+										</div>
+										<div className={titleStyling}>
+											<Typography variant={"h5"} component={"div"}>{item.title}</Typography>
+										</div>
+											<div className={textCartContainer}>
+												<Typography style={{fontSize: "20px", left: "20"}}>Pris: {item.price},-</Typography>
+												<div>
+												<div className={buttonStyling}>
+												<Button variant={"outlined"} onClick={() => updateItemQuantity(item.id, item.quantity -1)} style={{
+														color: 'black',
+														padding: "2px",
+														margin: "3px",
+													}}>-</Button>
+													<div style={{width: "50px", display: "inline-block"}}>
+														<span style={{
+															fontWeight: "bold",
+															position: "relative",
+															fontSize: "20px",
+															right: "19px"
+														}}>{item.quantity}</span>
+													</div>
+													<Button variant={"outlined"} onClick={() => updateItemQuantity(item.id, item.quantity +1)} style={{
+														color: 'black',
+														padding: "2px",
+														margin: "3px",
+													}}>+</Button>
+													<Button onClick={() => removeItem(item.id)} style={{
+														color: 'white',
+														backgroundColor: '#d36161',
+														left: "2rem",
+													}}>Fjern vare</Button>
+												</div>
+													
+													<Divider className={dividerStyling}/>
+												</div>
+											</div>
+									</div>
+								)
+							})}
+						</div>
+					</div>
+				</div>
+				<div className={totalPriceStyling}>
+					<Typography style={{fontSize: "20px"}}>Totalsum: <b>{cartTotal}</b> kr (evt. frakt kalkuleres i kassen)</Typography>
+				</div>
+				<div>
+					<Button size={"small"} onClick={() => emptyCart()} style={{
+						color: 'white',
+						backgroundColor: '#006357',
+					}}>Tøm handlevognen</Button>
+				</div>
+			</div>
+		</section>
+
+			/*<ModalCart/>*/
+	);
+};
+
+export default MobileCart;
+
+export class totalUniqueItems {
+}
--- src/components/Cart/ModalCart.js
+++ src/components/Cart/ModalCart.js
@@ -5,51 +5,83 @@ import Modal from '@mui/material/Modal';
 import ShopIcon from '@mui/icons-material/Shop';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Cart from "./Cart.js";
+import MobileCart from "./MobileCart.js";
 import {Link} from "react-router-dom";
+import {useMediaQuery, useTheme} from "@material-ui/core";
 
 const style = {
-    position: 'relative',
-    borderRadius: '12px',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: 710,
-    bgcolor: 'background.paper',
-    border: '1px solid #000',
-    boxShadow: 24,
-    p: 4
+	position: 'relative',
+	borderRadius: '12px',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	maxWidth: 710,
+	
+	bgcolor: 'background.paper',
+	border: '1px solid #000',
+	boxShadow: 24,
+	p: 4
 };
 
 
 function ModalCart({open, onClose}) {
 
-    return (
-        <div>
-            <Modal style={{overflow: "scroll"}} open={open} onClose={onClose} aria-labelledby="modal-modal-title"
-                   aria-describedby="modal-modal-description">
-                <Box sx={style}>
-                    <Cart/>
+	
+	const theme = useTheme();
+    const isMatch = useMediaQuery(theme.breakpoints.down('md'));
 
-                    <Button onClick={onClose} component={Link} to="/pizza" style={{
-                        color: 'white',
-                        backgroundColor: '#006357',
-                        position: 'relative', top: '20px', left: '17%'
-                    }} variant="outlined" endIcon={<AddShoppingCartIcon/>} size={"large"}>
-                        Fortsett å handle
-                    </Button>
+	return (
+		<div>
+			{isMatch ? <Modal style={{overflow: "scroll"}} open={open} onClose={onClose} aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description">
+				<Box sx={style}>
+					<MobileCart/>
 
-                    <Button onClick={onClose} component={Link} to="/checkOut" style={{
-                        color: 'black',
-                        backgroundColor: '#FFD148',
-                        position: 'relative', top: '20px', left: '30%', margin: "20px"
-                    }} variant="outlined" endIcon={<ShopIcon/>} size={"large"}>
-                        Checkout
-                    </Button>
+					<Button onClick={onClose} component={Link} to="/pizza" style={{
+						color: 'white',
+						backgroundColor: '#006357',
+						position: 'relative', top: '20px', left: '17%'
+					}} variant="outlined" endIcon={<AddShoppingCartIcon/>} size={"large"}>
+						Fitte å fitte
+					</Button>
 
-                </Box>
-            </Modal>
-        </div>
-    );
+					<Button onClick={onClose} component={Link} to="/checkOut" style={{
+						color: 'black',
+						backgroundColor: '#FFD148',
+						position: 'relative', top: '20px', left: '30%', margin: "20px"
+					}} variant="outlined" endIcon={<ShopIcon/>} size={"large"}>
+						Fitte
+					</Button>
+
+				</Box>
+			</Modal>: (
+					<Modal style={{overflow: "scroll"}} open={open} onClose={onClose} aria-labelledby="modal-modal-title"
+					aria-describedby="modal-modal-description">
+				<Box sx={style}>
+					<Cart/>
+
+					<Button onClick={onClose} component={Link} to="/pizza" style={{
+						color: 'white',
+						backgroundColor: '#006357',
+						position: 'relative', top: '20px', left: '17%'
+					}} variant="outlined" endIcon={<AddShoppingCartIcon/>} size={"large"}>
+						Fortsett å handle
+					</Button>
+
+					<Button onClick={onClose} component={Link} to="/checkOut" style={{
+						color: 'black',
+						backgroundColor: '#FFD148',
+						position: 'relative', top: '20px', left: '30%', margin: "20px"
+					}} variant="outlined" endIcon={<ShopIcon/>} size={"large"}>
+						Checkout
+					</Button>
+
+				</Box>
+			</Modal>
+			)}
+				
+		</div>
+	);
 }
 
 
