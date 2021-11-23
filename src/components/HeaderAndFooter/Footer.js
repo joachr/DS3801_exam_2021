fb2ea20756b48bb7d5100c5b6acf55eae5fcce79
Joachim AC <joacchri@gmail.com>
2021-11-23 23:02:03 +0000

    responsive footer good to go

--- src/components/HeaderAndFooter/Footer.js
+++ src/components/HeaderAndFooter/Footer.js
@@ -107,56 +107,8 @@ function Footer() {
                     <FacebookIcon style={{marginTop: '20px', marginLeft: '30px', fontSize: '70px', color: '#4267B2'}}/>
                     <InstagramIcon style={{marginTop: '20px', fontSize: '70px', color: '#E1306C'}}/>
                     <TwitterIcon style={{marginTop: '20px', fontSize: '70px', color: '#1DA1F2'}}/>
-                </BottomNavigation> : (
-                    <BottomNavigation style={{
-                        position: 'fixed',
-                        bottom: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '120px',
-                        backgroundColor: '#FFD148',
-                        marginTop: '150px'
-                    }}>
-                        <Box style={{
-                            height: '100%',
-                            width: '20rem',
-                            backgroundColor: '#006357',
-                        }}>
-                            <div style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                marginTop: '1rem'
-                            }}>
-                                <ul style={{
-                                    listStyle: 'none',
-                                    color: 'white',
-                                    fontSize: 'large',
-                                }}>
-                                    <li style={{
-                                        marginTop: '0.5rem'
-                                    }}>Tlf: 90 34 34 34
-                                    </li>
-                                    <li style={{
-                                        marginTop: '0.5rem'
-                                    }}>E-post: strandpizza@gmail.com
-                                    </li>
-                                    <li style={{
-                                        marginTop: '0.5rem'
-                                    }}>Adresse: Strandveien 4, 0000, Strand
-                                    </li>
-                                </ul>
-                            </div>
-                        </Box>
-
-                        <FacebookIcon
-                            style={{marginTop: '20px', marginLeft: '400px', fontSize: '90px', color: '#4267B2'}}/>
-                        <InstagramIcon style={{marginTop: '20px', fontSize: '90px', color: '#E1306C'}}/>
-                        <TwitterIcon style={{marginTop: '20px', fontSize: '90px', color: '#1DA1F2'}}/>
-                    </BottomNavigation>
-                )}
+                </BottomNavigation>)} 
         </div>
-
     )
 }
 
